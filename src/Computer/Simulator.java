144998ac0c8f6d76533055e538e86da7187d4c94
QiTianYi <qitianyi@live.com>
2019-09-19 13:25:57 -0400

    Add Comments to Simulator and Memory.

--- src/Computer/Simulator.java
+++ src/Computer/Simulator.java
@@ -12,6 +12,9 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 
+/**
+ * The main simulator class.
+ */
 
 public class Simulator {
 
@@ -30,6 +33,9 @@ public class Simulator {
 
     }
 
+    /**
+     * initialize all components.
+     */
     public void initialize() {
         componets = new Componets();
         DataMemory = new Memory();
@@ -37,7 +43,23 @@ public class Simulator {
         BUS = new Bus(componets, DataMemory);
     }
 
-
+    /**
+     * Load the memory form a file. (CSV file).
+     * LOAD MEM expects a CSV file which contains formatted MEM data. It should have 2 fields, which are
+     * <p>
+     * <Memory Location(Binary String)>,<Data(Binary String)>.
+     * <p>
+     * Here is an example:
+     * <p>
+     * 000000000001,0000011100011111
+     * 000000000010,0000011100011111
+     * 000000000011,0000011100011111
+     * 000000000100,0000011100011111
+     * <p>
+     * The example above load data "0000011100011111" to memory location 01,10,11,100.
+     *
+     * @param file the file of the memory log.
+     */
     public void loadMEMfromFile(File file) {
         try {
             int i = 0;
@@ -46,8 +68,9 @@ public class Simulator {
             BufferedReader br = new BufferedReader(reader);
             String line = null;
             while ((line = br.readLine()) != null) {
+                // Split the line with ","
                 String[] buff = line.split(",");
-                //buff[0]=address;buff[1]=data;
+                // buff[0] is the address; buff[1] is the data;
                 DataMemory.UserSet(buff[0], buff[1], true);
                 i++;
             }
@@ -58,99 +81,4 @@ public class Simulator {
     }
 }
 
-    //// user should have right to chose where their file come from
-//    public void bootLoader() {
-//        String txtPath = "C:\\Users\\QiTianYi\\Desktop\\UserProgram.txt";
-//        loadUserProgramFromTxt(txtPath);
-//        System.out.println("DEBUG: WE SET THE USER PROGRAM LOCATION TO 40(FIRST INSTR).");
-//        int i= 0;
-//        for(i=0;i<UserProgram.size();i++){
-//            DataMemory.set(40+i, (String) UserProgram.get(i));
-//        }
-//        componets.getPC().setValue(40);
-//        UserProgramLength = i;
-//        System.out.println("DEBUG: THE LENGTH OF THE USER RROGRAM:"+UserProgramLength);
-//
-//    }
-//
-//    private void loadUserProgramFromTxt(String txtPath){
-//        try {
-//            int i = 0;
-//            File file = new File(txtPath);
-//            InputStreamReader reader = new InputStreamReader(
-//                    new FileInputStream(file));
-//            BufferedReader br = new BufferedReader(reader);
-//            String line = null;
-//            while((line = br.readLine()) != null){
-//                if (line.length()>16){
-//                    line = line.substring(0,16);
-//                }
-//                UserProgram.add(line);
-//                i++;
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void RunUserProgram(){
-//        for(int i=0;i<UserProgramLength;i++){
-//            BUS.tik();
-//        }
-//    }
-//
-//    public void SingleStepUserProgram(){
-//        BUS.tik();
-//    }
-
-/*
-    //DEBUG Use only...
-    public static void main(String[] args) {
-
-
-        Simulator simulator = new Simulator();
-        simulator.DataMemory.set(31, 666);
-        simulator.DataMemory.set(666, 123);
-
-
-        //TESTING AUTORUN...
-        System.out.println("Testing Autotest...");
-        simulator.DataMemory.set(2000, "0000011100111111");
-        simulator.DataMemory.set(2001, "0000011000111111");
-        simulator.DataMemory.set(2002, "0000010100111111");
-        simulator.DataMemory.set(2003, "0000010000111111");
-        simulator.componets.PC.setValue(2000);
-        int i = 0;
-        for (i = 0; i < 4; i++) {
-            simulator.BUS.tik();
-        }
-
-
-        //LDR
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000011100111111", 2));
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000011000111111", 2));
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000010100111111", 2));
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000010000111111", 2));
-        //STR
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000101100011111", 2));
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000101000011111", 2));
-        //LDA
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000111100011111", 2));
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000111000111111", 2));
-        //LDX
-        simulator.DataMemory.set(30, 789);
-        simulator.BUS.evaulateInstruction(Integer.valueOf("1010010001011110", 2));
-        //STX
-        simulator.componets.IX1.setValue(987);
-        simulator.BUS.evaulateInstruction(Integer.valueOf("1010100001011110", 2));
-        //NOP
-        simulator.BUS.evaulateInstruction(Integer.valueOf("0000000000000000", 2));
-    }
-
-*/
-//
-//    public static void main(String[] args) {
-//        Simulator simulator = new Simulator();
-//        simulator.bootLoader();
-//    }
-//}
 
