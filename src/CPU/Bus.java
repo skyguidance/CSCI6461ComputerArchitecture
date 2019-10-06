592f42e8a35f8e3e589f25b841ac3c43495f019f
QiTianYi <qitianyi@live.com>
2019-10-06 10:34:55 -0400

    Support OverflowSet to Register. Instr 04 Tested. Accpeted.

--- src/CPU/Bus.java
+++ src/CPU/Bus.java
@@ -154,7 +154,7 @@ public class Bus {
                 // AMR. Add Memory To Register.
                 componets.ALU.Calc(componets.getCU().getOpcode(), componets.getGPRRegister().getValue(), dataMemory.get(ea));
                 componets.CC0.set(componets.ALU.CC0);
-                componets.getGPRRegister().setValue(componets.ALU.output);
+                componets.getGPRRegister().setValue(componets.ALU.output,componets.ALU.CC0);
                 break;
             }
             case 5: {
--- src/CPU/Register.java
+++ src/CPU/Register.java
@@ -29,6 +29,13 @@ public class Register {
         return String.format(format, Long.valueOf(a));//
     }
 
+    public String ToBinaryString(int value,int length) {
+        String a = Integer.toBinaryString(value);// Change to BinaryString
+        String Stringlength = "" + length;
+        String format = "%0numberd".replace("number", Stringlength);
+        return String.format(format, Long.valueOf(a));//
+    }
+
     /**
      * Get the Register value.
      *
@@ -64,6 +71,36 @@ public class Register {
     public void setValue(String Value) {
         int IntValue = Integer.valueOf(Value, 2);
         setValue(IntValue);
+    }
+
+    /**
+     * Do the security check (if overflow) and set the value to the register.
+     * If the value is not legit, log an INVALID operation.
+     * If the OverFlowSet is TRUE, then set the overflow value to Register.
+     *
+     * Ex. If the overflow is a 18 digit number (in binary). And the OverflowSet is True.
+     * Set the lower 16 bit of the 18 digit number to the Register.
+     *
+     * @param Value Integer. The int value to be set to the register.
+     * @param OverFlowSet Boolean. If is set to true, Set the lower bit of the overflow value.
+     */
+    public void setValue(int Value,boolean OverFlowSet) {
+        if (Math.pow(2, length) > Value && Value >= 0) {
+            this.value = Value;
+            logging.info(name + "=>" + ToBinaryString() + "(" + value + ")");
+            System.out.println(name + "=>" + ToBinaryString() + "(" + value + ")");
+        } else {
+            if(OverFlowSet){
+                // The value is overflow and need to set the lower bit.
+                String StringValue = ToBinaryString(Value,32);
+                StringValue = StringValue.substring(StringValue.length()-length);
+                setValue(StringValue);
+            }
+            else{
+                logging.info("INVALID " + name + "=>" + ToBinaryString() + "(" + Value + ")");
+                System.out.println("INVALID " + name + "=>" + ToBinaryString() + "(" + Value + ")");
+            }
+        }
     }
 
 }
