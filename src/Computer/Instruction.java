f0b755728b4130eab9f7b44051e4a8c9a19f1434
jinchengyang <jincheng.yang@hope.edu>
2019-10-09 20:42:58 -0400

    Make a instruction enum class we might use it

---
+++ src/Computer/Instruction.java
@@ -0,0 +1,49 @@
+package Computer;
+
+public enum Instruction {
+    HALT(0),
+    LDR(1),
+    STR(2),
+    LDA(3),
+    AMR(4),
+    SMR(5),
+    AIR(6),
+    SIR(7),
+    JZ(10),
+    JNE(11),
+    JCC(12),
+    JMA(13),
+    JSR(14),
+    RFS(15),
+    SOB(16),
+    JGE(17),
+    MLT(20),
+    DVD(21),
+    TRR(22),
+    AND(23),
+    ORR(23),
+    NOT(25),
+    SRC(31),
+    RRC(32),
+    FADD(33),
+    FSUB(34),
+    VADD(35),
+    VSUB(36),
+    CNVRT(37),
+    TRAP(36),
+    LDX(41),
+    STX(42),
+    LDFR(50),
+    STFR(51),
+    IN(61),
+    OUT(62),
+    CHK(63)
+    ;
+    private  final String opcode;
+    private Instruction(int value){
+        opcode=Integer.toBinaryString(value);
+    }
+    public String getOpcode(){
+        return opcode;
+    }
+}
